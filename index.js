cfbee0bd93aa923f591944618a7f7395c3ad9734
valeska <himurayue23@gmail.com>
2026-03-04 11:16:13 -0400

    primer commit de la part3

--- index.js
+++ index.js
@@ -1,119 +1,127 @@
-// const http = require('http')
 
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
-app.use(express.json())
-const cors = require('cors')
 
 
-app.use(cors())
 
-let notes = [
+
+let phoneAddress = [
   {
-    id: 1,
-    content: "HTML is easy",
-    important: true
+    "id": 1,
+    "name": "Arto Hellas",
+    "number": "040-123456"
   },
   {
-    id: 2,
-    content: "Browser can execute only JavaScript",
-    important: false
+    "id": 2,
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523"
   },
   {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
+    "id": 3,
+    "name": "Dan Abramov",
+    "number": "12-43-234345"
+  },
+  {
+    "id": 4,
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122"
   }
 ]
-// const app = http.createServer((request, response) => {
-//   response.writeHead(200, { 'Content-Type': 'application/json' })
-//   response.end(JSON.stringify(notes))
-// })
 
 
-const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method)
-  console.log('Path:  ', request.path)
-  console.log('Body:  ', request.body)
-  next()
-}
+app.use(express.json())
 
-app.use(requestLogger)
+app.use(morgan('tiny'))
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
-})
-
-// funcion para ver todos los recursos
-app.get('/api/notes', (request, response) => {
-  response.json(notes)
+// funcion para obtener todos los recursos
+app.get('/api/phoneaddress', (request, response) => {
+  response.json(phoneAddress)
 })
 
 
-// funcion para agregar un nuevo recurso
-
-const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
-    : 0
-  return maxId + 1
-}
-app.post('/api/notes', (request, response) => {
-  const body = request.body
-
-  if (!body.content) {
-    return response.status(400).json({
-      error: 'content missing'
-    })
-  }
-
-  const note = {
-    content: body.content,
-    important: Boolean(body.important) || false,
-    id: generateId(),
-  }
-
-  notes = notes.concat(note)
-
-  response.json(note)
-})
-
-
-// funcion para ver recursos individuales
-app.get('/api/notes/:id', (request, response) => {
+// Funcion para obtener un recurso
+app.get('/api/phoneaddress/:id', (request, response) => {
   const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
-  if (note) {
-    response.json(note)
-  } else {
 
+  const person = phoneAddress.find(p => p.id === id)
+
+  if (person) {
+    response.json(person)
+  } else {
     response.status(404).send("404 Pagina no encontrada")
   }
 })
 
+const generateId = () => {
+  const maxId = phoneAddress.length > 0
+    ? Math.max(...phoneAddress.map(p => p.id))
+    : 0
+  return maxId + 1
+}
 
-// funfion para borrar un recurso
-app.delete('/api/notes/:id', (request, response) => {
+app.post('/api/phoneaddress', (request, response) => {
+
+  const body = request.body
+  const nameExists = phoneAddress.find(p => p.name === body.name)
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: 'name or number is missing'
+    })
+  }
+
+
+
+  if (nameExists) {
+    return response.status(400).json({
+      error: 'name must be unique'
+    })
+  }
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId()
+  }
+
+  phoneAddress = phoneAddress.concat(person)
+  response.json(person)
+
+})
+
+// Funcion para obtener informacion del array (logitud y fecha)
+app.get('/info', (request, response) => {
+  const recursos = phoneAddress.length
+  const fecha = new Date()
+
+  const formatoLocal = new Intl.DateTimeFormat('es-VE', {
+    dateStyle: 'full',
+    timeStyle: 'medium'
+  }).format(fecha);
+
+
+  response.send(`PhoneBook has info for ${recursos} people. <br/>${formatoLocal}`);
+
+})
+
+// Funcion para borrar un recurso
+app.delete('/api/phoneaddress/:id', (request, response) => {
   const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
+  phoneAddress = phoneAddress.filter(p => p.id !== id)
 
   response.status(204).end()
 })
 
 
+// Definimos un nuevo token llamado 'body'
+morgan.token('body', (req) => JSON.stringify(req.body))
 
+// Usamos un formato personalizado que incluya el token :body
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-
-
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
-
-app.use(unknownEndpoint)
-
-
-const PORT = process.env.PORT || 3001
+const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
+
 })
